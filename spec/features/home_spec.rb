5186684c7185f1f99dbef2e3e50ad9a9063f9d7f
Robson Barbosa <robsons@alunos.utfpr.edu.br>
2020-12-20 17:55:58 -0300

    Setup Rspec for tests

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe "Home", type: :feature do
+
+  it "have a title" do
+    visit root_path
+
+    expect(page).to have_text("Sistema para Gestão de Certificados e Declarações da UTFPR-GP")
+  end
+end
